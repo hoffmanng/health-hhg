546994f7e6c948765f6bcf04bc0f2077e69a4a9a
Gergely Hoffmann <gergely.hoffmann02@sap.com>
2021-06-19 22:11:14 +0200

    refactor: use env variables

--- src/api/auth/authentication.controller.js
+++ src/api/auth/authentication.controller.js
@@ -1,6 +1,4 @@
 const jwt = require('jsonwebtoken');
-const jwtSecret = require('../../common/env.config.js').jwt_secret;
-const jwtValidityInSec = require('../../common/env.config.js').jwt_expiration_in_seconds;
 const UsersService = require('../users/users.service');
 const AuthHelper = require('../../common/auth.helper');
 
@@ -47,9 +45,8 @@ exports.validateUserAndPassword = async (req, res, next) => {
 exports.login = async (req, res, next) => {
     try {
         const user = await UsersService.findByEmail({ email: req.body.email });
-        const accessToken = jwt.sign(user, jwtSecret);
-        const cookieExpirationTime = new Date(Date.now() + jwtValidityInSec * 1000);
-        res.cookie('jwt', accessToken, { expires: cookieExpirationTime, httpOnly: true });
+        const accessToken = jwt.sign(user, process.env.JWT_SECRET);
+        const cookieExpirationTime = new Date(Date.now() + Number(process.env.JWT_EXPIRATION_IN_SEC) * 1000);
         res.status(200).send({ accessToken });
     } catch (err) {
         console.log(err.message);
@@ -93,7 +90,7 @@ exports.validJWTNeeded = (req, res, next) => {
         }
         const jwtToken = jwtCookie || req.headers.authorization.split(' ')[1];
         // 403
-        req.jwt = jwt.verify(jwtToken, jwtSecret);
+        req.jwt = jwt.verify(jwtToken, process.env.JWT_SECRET);
 
         if (req.jwt.expireAt && req.jwt.expireAt < Date.now() / 1000) {
             // 401
